893195b53c5a054b97f57fa8d6ad26ccde8eb5a9
Yi Fei <yifeiyangyang@gmail.com>
2024-05-30 01:31:18 +0800

    hi

---
+++ frontend/src/navigation/MainTabNavigator.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from '../screens/HomeScreen';
+import SwipingScreen from '../screens/SwipingScreen';
+import ChatsScreen from '../screens/ChatsScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import PostsScreen from '../screens/PostsScreen';
+
+const Tab = createBottomTabNavigator();
+
+const MainTabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Swiping" component={SwipingScreen} />
+      <Tab.Screen name="Chats" component={ChatsScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Posts" component={PostsScreen} />
+    </Tab.Navigator>
+  );
+};
+
+export default MainTabNavigator;
